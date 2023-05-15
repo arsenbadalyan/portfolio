bce5672f7a62c8afc9fe97d0e89e5ff1a403a8fe
Arsen Badalyan <arsenbadalyan.01@gmail.com>
2023-05-15 23:13:55 +0400

    feat: portfolio init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Content from './Content';
+import Sidebar from './Sidebar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App() 
+{
+	return (
+		<div className="App font-mono">
+            <div className="flex">
+                <div className="w-[20vw]">
+                    <Sidebar/>
+                </div>
+                <div className="w-[80vw]">
+                    <Content/>
+                </div>
+            </div>
+		</div>
+	);
 }
 
 export default App;
